d2bb380482b1df43384ecaacb9fa86a59f98bfc2
raviayani00 <raviayani7@gmail.com>
2026-05-28 22:27:40 +0530

    Update script.js

--- RaviAyani_Task1/script.js
+++ RaviAyani_Task1/script.js
@@ -1,1 +1,16 @@
+function scrollTo(id) {
+    document.getElementById(id).scrollIntoView();
+}
 
+function showMsg() {
+    alert('Thanks for contacting! We will reply soon.');
+}
+
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        nav.style.background = '#0f0f1e';
+    } else {
+        nav.style.background = '#1a1a2e';
+    }
+});
